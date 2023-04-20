a863671aa106eadd62b954c9f6a05f2a5db6901d
villa_mou <mouxuefei@edocyun.com.cn>
2023-04-20 17:15:35 +0800

    chore

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import * as FaceDetector from "expo-face-detector";
-import { captureRef } from "react-native-view-shot";
-import React, { ReactElement, useEffect, useRef, useState } from "react";
-import dayjs from "dayjs";
+import * as FaceDetector from 'expo-face-detector'
+import { captureRef } from 'react-native-view-shot'
+import React, { ReactElement, useEffect, useRef, useState } from 'react'
 import {
   Button,
   StyleSheet,
@@ -9,111 +8,111 @@ import {
   View,
   Image,
   ToastAndroid,
-  Platform,
-} from "react-native";
-import Rectangle from "./Rectangle.js";
-import { Camera } from "expo-camera";
-import { BlurView } from "expo-blur";
-import * as MediaLibrary from "expo-media-library";
+  Platform
+} from 'react-native'
+import Rectangle from './Rectangle.js'
+import { Camera } from 'expo-camera'
+import { BlurView } from 'expo-blur'
+import * as MediaLibrary from 'expo-media-library'
 
-export default function App() {
-  const [permission, setPermission] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(0);
-  const [windowHeight, setWindowHeight] = useState(0);
-  const [posGroup, setPosGroup] = useState(null);
-  const [showCameraPage, setShowCameraPage] = useState(true);
-  const cameraRef = useRef(undefined);
-  const imageContainerRef = useRef(undefined);
-  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
-  const [propsFaceDetector, setPropsFaceDetector] = useState(null);
-  const [firstImage, setFirstImage] = useState(null);
-  const [firstImagePos, setFirstImagePos] = useState(null);
-  const [cameraType, setCameraType] = useState("front");
-  const msgRef = useRef(undefined);
+export default function App () {
+  const [permission, setPermission] = useState(false)
+  const [windowWidth, setWindowWidth] = useState(0)
+  const [windowHeight, setWindowHeight] = useState(0)
+  const [posGroup, setPosGroup] = useState(null)
+  const [showCameraPage, setShowCameraPage] = useState(true)
+  const cameraRef = useRef(undefined)
+  const imageContainerRef = useRef(undefined)
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions()
+  const [propsFaceDetector, setPropsFaceDetector] = useState(null)
+  const [firstImage, setFirstImage] = useState(null)
+  const [firstImagePos, setFirstImagePos] = useState(null)
+  const [cameraType, setCameraType] = useState('front')
+  const msgRef = useRef(undefined)
   useEffect(() => {
     (async () => {
-      await requestPermission();
-      const { status } = await Camera.requestMicrophonePermissionsAsync();
-      setPermission(status === "granted");
-    })();
-  }, []);
+      await requestPermission()
+      const { status } = await Camera.requestMicrophonePermissionsAsync()
+      setPermission(status === 'granted')
+    })()
+  }, [])
 
   if (!permission) {
-    return <Text>No access to camera</Text>;
+    return <Text>No access to camera</Text>
   }
 
   const showToast = (msg) => {
     if (msgRef.current !== msg) {
-      if (Platform.OS === "android") {
-        msgRef.current = msg;
-        ToastAndroid.show(msg, ToastAndroid.SHORT);
+      if (Platform.OS === 'android') {
+        msgRef.current = msg
+        ToastAndroid.show(msg, ToastAndroid.SHORT)
       }
     }
-  };
+  }
 
   const handleDetectedFaces = ({ faces }) => {
     if (faces && faces[0]) {
-      console.log("faces[0]========", faces[0]);
-      const boundWidth = faces[0]?.bounds?.size?.width;
-      const boundHeight = faces[0]?.bounds?.size?.height;
-      const boundTop = faces[0]?.bounds?.origin?.y;
-      const boundLeft = faces[0]?.bounds?.origin?.x;
+      console.log('faces[0]========', faces[0])
+      const boundWidth = faces[0]?.bounds?.size?.width
+      const boundHeight = faces[0]?.bounds?.size?.height
+      const boundTop = faces[0]?.bounds?.origin?.y
+      const boundLeft = faces[0]?.bounds?.origin?.x
       const firstMask = {
         width: boundWidth / 2,
         height: boundHeight / 2,
         top: boundTop / 2,
-        left: boundLeft / 2,
-      };
-      setFirstImagePos(firstMask);
-      setPosGroup(faces[0]);
-      showToast("Face Detected!!");
+        left: boundLeft / 2
+      }
+      setFirstImagePos(firstMask)
+      setPosGroup(faces[0])
+      showToast('Face Detected!!')
     } else {
-      setFirstImagePos(undefined);
-      setPosGroup(undefined);
-      showToast("Captured images should contain a face!!");
+      setFirstImagePos(undefined)
+      setPosGroup(undefined)
+      showToast('Captured images should contain a face!!')
     }
-  };
+  }
 
   const onPressTake = async () => {
     try {
-      requestPermission();
-      const { uri } = await cameraRef.current?.takePictureAsync();
-      console.log("aaa===", uri);
-      const asset = await MediaLibrary.createAssetAsync(uri);
-      console.log("nbbbb===", asset);
-      setFirstImage({ type: cameraType, ...asset });
-      setShowCameraPage(false);
+      requestPermission()
+      const { uri } = await cameraRef.current?.takePictureAsync()
+      console.log('aaa===', uri)
+      const asset = await MediaLibrary.createAssetAsync(uri)
+      console.log('nbbbb===', asset)
+      setFirstImage({ type: cameraType, ...asset })
+      setShowCameraPage(false)
     } catch (error) {
-      console.log("erorr====", error);
+      console.log('erorr====', error)
     }
-  };
+  }
 
   const onPressChangeCamera = () => {
-    if (cameraType === "front") {
-      setCameraType("back");
+    if (cameraType === 'front') {
+      setCameraType('back')
     } else {
-      setCameraType("front");
+      setCameraType('front')
     }
-  };
+  }
 
   const onPressSave = async () => {
     try {
       const result = await captureRef(imageContainerRef, {
-        result: "tmpfile",
+        result: 'tmpfile',
         height: firstImage.height,
         width: firstImage.width,
         quality: 1,
-        format: "png",
-      });
-      console.log("result====", result);
-      const asset = await MediaLibrary.createAssetAsync(result);
-      console.log("nbbbb2===", asset);
+        format: 'png'
+      })
+      console.log('result====', result)
+      const asset = await MediaLibrary.createAssetAsync(result)
+      console.log('nbbbb2===', asset)
     } catch (error) {
-      console.log("erorr====", error);
+      console.log('erorr====', error)
     }
-  };
+  }
 
-  let contentView;
+  let contentView
 
   if (showCameraPage) {
     contentView = (
@@ -122,7 +121,7 @@ export default function App() {
           style={{ flex: 1 }}
           ref={cameraRef}
           type={cameraType}
-          ratio={"16:9"}
+          ratio={'16:9'}
           {...propsFaceDetector}
           onCameraReady={() => {
             setPropsFaceDetector({
@@ -132,31 +131,31 @@ export default function App() {
                 detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
                 runClassifications:
                   FaceDetector.FaceDetectorClassifications.none,
-                minDetectionInterval: 300,
-              },
-            });
+                minDetectionInterval: 300
+              }
+            })
           }}
         >
           <Rectangle face={posGroup} />
         </Camera>
         <View
           style={{
-            position: "absolute",
+            position: 'absolute',
             bottom: 20,
-            flexDirection: "row",
-            justifyContent: "space-between",
-            width: "100%",
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: '100%'
           }}
         >
           <Button title="take Photo" onPress={onPressTake}></Button>
           <Button title="change camera" onPress={onPressChangeCamera}></Button>
         </View>
       </View>
-    );
+    )
   } else {
     contentView = (
       <View style={{ flex: 1 }}>
-        <View style={{ flexDirection: "row" }}>
+        <View style={{ flexDirection: 'row' }}>
           <View
             style={{ aspectRatio: 9 / 16, width: windowWidth / 2 }}
             ref={imageContainerRef}
@@ -166,7 +165,7 @@ export default function App() {
                 aspectRatio: windowWidth / windowHeight,
                 width: windowWidth / 2,
                 transform:
-                  firstImage.type === "front" ? [{ rotateY: "180deg" }] : [],
+                  firstImage.type === 'front' ? [{ rotateY: '180deg' }] : []
               }}
               source={{ uri: firstImage.uri }}
             />
@@ -179,8 +178,8 @@ export default function App() {
           <Image
             style={{
               aspectRatio: windowWidth / windowHeight,
-              backgroundColor: "green",
-              width: windowWidth / 2,
+              backgroundColor: 'green',
+              width: windowWidth / 2
             }}
           />
         </View>
@@ -188,34 +187,34 @@ export default function App() {
           <Button title="save Photo" onPress={onPressSave}></Button>
         </View>
       </View>
-    );
+    )
   }
 
   return (
     <View
       style={styles.container}
       onLayout={(e) => {
-        const height = e.nativeEvent.layout.height;
-        const width = e.nativeEvent.layout.width;
+        const height = e.nativeEvent.layout.height
+        const width = e.nativeEvent.layout.width
         if (height > width) {
-          setWindowWidth(width);
-          setWindowHeight(height);
+          setWindowWidth(width)
+          setWindowHeight(height)
         } else {
-          setWindowWidth(height);
-          setWindowHeight(width);
+          setWindowWidth(height)
+          setWindowHeight(width)
         }
       }}
     >
       {contentView}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   blurContainer: {
-    position: "absolute",
-  },
-});
+    position: 'absolute'
+  }
+})
